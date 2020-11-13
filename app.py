3a56f2411fb0c6712c1f532b30dd4a6ddb1f2557
alt-nikitha <nikithasrikanth1711@gmail.com>
2020-11-13 23:05:06 +0530

    dropdown lists all newssources

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+import pandas as pd
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 
 def index():
-    return render_template('index.html')
+    dataset=pd.read_csv("final.csv")
+    news_sources=dataset['Name'].to_list()
+    
+    return render_template('index.html',ns=news_sources)
 
 
 if __name__=='__main__':
