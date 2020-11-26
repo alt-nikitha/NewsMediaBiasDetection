752484ac2f695f9cb267cd4fdd5a0556e59d80a9
alt-nikitha <nikithasrikanth1711@gmail.com>
2020-11-26 19:13:38 +0530

    added comments

--- article_labels.py
+++ article_labels.py
@@ -1,11 +1,21 @@
 import pandas as pd 
 
+# read the csv file with all articles
 df=pd.read_csv('all_articles.csv')
+
+# rename the news source column from publication to URL so that it can 
+# be joined with the news source csv file to get bias
 df.rename(columns = {" publication": "URL"},  
           inplace = True) 
+
+# read the news source with bias csv file
 df1=pd.read_csv('final.csv')
+
+#make sure data types are same for merge
 df['URL']=df['URL'].astype(str)
 df1['URL']=df1['URL'].astype(str)
+
+#some URLs do not have / at the end in both files, so append at the end
 for i in range(len(df)):
     val=df['URL'].iloc[i]
     if(val[-1]!='/'):
@@ -18,8 +28,8 @@ for i in range(len(df1)):
 
 
 
-# print(df.columns, df1.columns)
-
+# join both dataframes based on URL
 joined=df.merge(df1, on="URL", how="left", sort=False)
 
+#store final dataset of articles with bias labels
 joined.to_csv("articleswithlabels.csv")
