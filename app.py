25e97411a9f36efd3523fd0baddc823bbc3ed4f2
alt-nikitha <nikithasrikanth1711@gmail.com>
2020-12-07 11:52:03 +0530

    added model to flask

--- app.py
+++ app.py
@@ -5,6 +5,37 @@ import pymongo
 from pymongo import MongoClient 
 import newspaper 
 from newspaper import Article
+import torch
+import numpy as np
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', 
+                                          do_lower_case=True)
+                                          
+label_dict={'center': 5,
+ 'left': 2,
+ 'leftcenter': 1,
+ 'nan': 4,
+ 'right': 0,
+ 'right-center': 3}
+
+inverse_dic={
+ 5:'center',
+ 2:'left',
+ 1:'leftcenter',
+ 4:'nan',
+ 0:'right',
+ 3:'right-center'
+}
+
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased",
+                                                      num_labels=len(label_dict),
+                                                      output_attentions=False,
+                                                      output_hidden_states=False)
+device=torch.device('cpu')
+model.to(device)
+model.load_state_dict(torch.load('finetuned_BERT_epoch_5.model',map_location=torch.device('cpu')))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/nmbd'
@@ -58,17 +89,44 @@ def detect():
     date=article.publish_date
     text= article.text
 
+
+    # textsplit=text.split(' ')
+    # print(len(textsplit))
+    # if(len(textsplit)>512):
+    #     limittext=textsplit[:512]
+    #     print(len(limittext))
+    #     newtext=' '.join(limittext)
+    # else:
+    #     newtext=text
+    
+    inputs = tokenizer(text, padding='max_length', return_tensors='pt',truncation=True)
+    # print(len(inputs))
+    model.eval()
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+    pred_labels = np.argmax(outputs[0].cpu().detach().numpy(), axis=1).tolist()
+
     ##Post is the data you want to enter into the database. Not specifying an ID generates a random one 
     post= {"author": author[0], "source":article.source_url, "content":text,"date":date}
-
     collection.insert_one(post)
+
+    # textsplit=text.split('.')
+    # print(len(textsplit))
+    # if(len(textsplit)>20):
+    #     limittext=textsplit[:20]
+    #     # print(len(limittext))
+    #     newtext='.'.join(limittext)
+    # else:
+    #     newtext=text
+    
     context=dict(
         ns="haha",
-        bias="left",
+        bias=inverse_dic[pred_labels[0]],
         URL=url,
         author=author,
         date=date,
-        text=text,
+        # text=newtext,
         flag=1
     )
     return render_template('results.html', **context)
