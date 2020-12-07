9942f286aa04bf3cf3ff553a0b2dc615a04329ed
neelbhandari6 <30409992+neelbhandari6@users.noreply.github.com>
2020-12-07 12:10:40 +0530

    Corrected for summary feature

--- trial_mongo.py
+++ trial_mongo.py
@@ -2,6 +2,7 @@ import pymongo
 from pymongo import MongoClient 
 import newspaper 
 from newspaper import Article
+from newspaper import nlp
 ## This is my cluster , which is linked to my specific IP address
 cluster=MongoClient("mongodb+srv://neelb:NewsBiasDetect246@newsbiasdetection.bxxdo.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db=cluster["test"] ##database name
@@ -18,10 +19,12 @@ article.parse()
 author =article.authors
 date=article.publish_date
 text= article.text
+article.nlp()
 summary= article.summary
+print(article.summary)
 title= article.title
 
 ##Post is the data you want to enter into the database. Not specifying an ID generates a random one 
-post= {"_id":2,"author": author[0], "source":article.source_url, "content":text,"date":date,"summary":summary,"title":title}
+post= {"author": author[0], "source":article.source_url, "content":text,"date":date,"summary":summary,"title":title}
 
 collection.insert_one(post)
